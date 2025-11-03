fix: bug of fidelity_levels calculation (divided by self.R)

Optimizer/scheduler/fidelity.py
@@ -56,7 +56,7 @@ class BOHBFidelityScheduler(BaseScheduler):
         self.B = (self.s_max + 1) * self.R
         self.s_values = list(reversed(range(self.s_max + 1)))
 
-        self.fidelity_levels = [int(x) for x in np.logspace(0, self.s_max, self.s_max + 1, base=self.eta)]
+        self.fidelity_levels = [round(x / self.R, 5) for x in np.logspace(0, self.s_max, self.s_max + 1, base=self.eta)]
         assert len(self.fidelity_levels) == self.s_max + 1, "Fidelity levels length mismatch"
     
         logger.info("FidelityScheduler: run %d brackets with fidelity levels %s. s_max = [%d]. R = [%d], eta = [%d]" 