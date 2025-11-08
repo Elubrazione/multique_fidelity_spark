a4a658e3f1456489d98983bab6e9e66d378bc47f
jelly <1355532189@qq.com>
2025-11-09 00:07:10 +0800

    refactor: REMBO and HesBO projection compression (compression steps)

---
+++ Compressor/steps/projection/__init__.py
@@ -0,0 +1,10 @@
+from .base import TransformativeProjectionStep
+from .rembo import REMBOProjectionStep
+from .hesbo import HesBOProjectionStep
+
+__all__ = [
+    'TransformativeProjectionStep',
+    'REMBOProjectionStep',
+    'HesBOProjectionStep',
+]
+
