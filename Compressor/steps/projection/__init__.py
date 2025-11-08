7db1081febf2735e92b7bc91959fee5cae3df100
jelly <1355532189@qq.com>
2025-11-09 02:10:46 +0800

    refactor: classify quantization from range into projection step

--- Compressor/steps/projection/__init__.py
+++ Compressor/steps/projection/__init__.py
@@ -2,11 +2,13 @@ from .base import TransformativeProjectionStep
 from .rembo import REMBOProjectionStep
 from .hesbo import HesBOProjectionStep
 from .kpca import KPCAProjectionStep
+from .quantization import QuantizationProjectionStep
 
 __all__ = [
     'TransformativeProjectionStep',
     'REMBOProjectionStep',
     'HesBOProjectionStep',
     'KPCAProjectionStep',
+    'QuantizationProjectionStep',
 ]
 
